af0cf0cb4082749acdaf73ef7cd6357712c099bf
Kayla Gervais <61392996+kgervais@users.noreply.github.com>
2021-11-08 18:16:48 -0500

    Add files via upload

---
+++ Bootstrap, CSS and JS Lab/scripts.js
@@ -0,0 +1,2 @@
+// document.write("<footer class='text-light bg-dark'>Thank you!</footer>");
+document.getElementById("theFooter").innerHTML = "<footer class='text-light bg-dark'>Thank you!</footer>";
